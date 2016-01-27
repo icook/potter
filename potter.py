8e4d20a2b8cb153138fbb298cc14466cda70df25
Isaac Cook <isaac@simpload.com>
2016-01-27 00:26:49 -0600

    Pull possible cache images in run. Wrap images as objects
    * Disable cache after the first step that doesn't use it runs
    * Hold all information about generated Images in an object

--- potter.py
+++ potter.py
@@ -46,6 +46,15 @@ class Run(object):
 
         builtins = dict(pull=Pull, command=Command, copy=Copy)
         image = None
+
+        # Lookup all potential caching candidates
+        resps = self.client.images(all=True, filters={'label': "potter_name={}".format(self.config['name'])})
+        cache_images = {}
+        for resp in resps:
+            image = Image(resp, cache=True)
+            cache_images.setdefault(image.step, []).append(image)
+
+        cache_enabled = True
         for i, step in enumerate(self.config['steps']):
             typ = step.pop('type')
             step_cls = builtins.get(typ)
@@ -53,18 +62,53 @@ class Run(object):
                 logger.error("{} is an invalid step type".format(typ))
                 return False
 
-            step_obj = step_cls(self, step, i, image)
+            cache_objs = cache_images.get(i, []) if cache_enabled else []
+            step_obj = step_cls(self, step, i, image, cache_objs)
             image = step_obj.execute()
+            if image.cache is False:
+                cache_enabled = False
 
-        self.log("=====> Created {} in {}".format(image[:12], time.time() - start), color='OKGREEN')
+        self.log("=====> Created image {} in {}".format(image, time.time() - start), color='OKGREEN')
+
+
+class Image(object):
+    """ A wrapper for images that potter has generated """
+    def __init__(self, resp, cache=False):
+        self.id = resp.pop('Id')
+        self.created = datetime.datetime.utcfromtimestamp(resp.pop('Created', 0))
+        self.extra = resp
+        self.cache = cache  # Is this image cached?
+
+        labels = {key[7:]: val for key, val in resp['Labels'].items() if key.startswith("potter_")}
+        self.config = json.loads(labels.pop('config'))
+        self.config_hash = labels.pop('config_hash')
+        self.step = int(labels.pop('step'))
+        self.runtime = float(labels.pop('runtime', 0))
+        self.potter_labels = labels
+
+        assert len(self.id) == 64
+
+    @classmethod
+    def from_inspect(cls, resp):
+        new_resp = resp['Config']
+        new_resp['Id'] = resp['Id']
+        obj = cls(new_resp)
+        without_milli = resp['Created'].rsplit(".", 1)[0]
+        obj.created = datetime.datetime.strptime(without_milli, "%Y-%m-%dT%H:%M:%S")
+        return obj
+
+    def __str__(self):
+        return "<Image {}>".format(self.id[:12])
 
 
 class Step(object):
-    def __init__(self, run, config, step_num, target_image):
+    def __init__(self, run, config, step_num, target_image, cached_images):
         self.target_image = target_image
         self.step_num = step_num
         self.run = run
         self.config = config
+        self.cached_images = cached_images
+
         self.start_time = time.time()
         self.cacheable = True
         self.labels = self.gen_labels()
@@ -72,17 +116,6 @@ class Step(object):
         self.run.log("==> Step {} {} cfg:{}".format(
             step_num, self.__class__.__name__, self.config), color="HEADER")
 
-    def can_cache(self):
-        """ Check if there is a cache, and if so, should we use it? """
-        info = self.run.client.images(all=True, filters={'label': "potter-key={}".format(self.labels['potter-key'])})
-        if not info:
-            return False
-
-        info = dict(id=info[0]['Id'],
-                    config_hash=info[0]['Labels'].get('potter-config-hash'),
-                    created=datetime.datetime.utcfromtimestamp(info[0]['Created']),
-                    runtime=float(info[0]['Labels'].get('potter-runtime')))
-        logger.info("Found cache for step. {}".format(info))
     def gen_labels(self):
         return {
             "potter_name": self.run.config['name'],
@@ -91,30 +124,37 @@ class Step(object):
             "potter_config": json.dumps(self.config)
         }
 
-        # Determine cache usage by step config
-        if info['config_hash'] != self.labels['potter-config-hash']:
-            logger.info("Skipping cache because step configuration has changed")
+    def valid_cache(self, image):
+        """ Check if this image is a valid cached version of this step """
+        if image.config_hash != self.config_hash:
+            self.run.debug("Skipping {} cache because step configuration has changed"
+                         .format(image))
             return False
 
         if self.config.get('nocache') is True:
-            logger.info("Skipping cache because nocache flag")
+            self.run.debug("Skipping {} cache because nocache flag".format(image))
             return False
 
         invalidate_after = self.config.get('invalidate_after')
         if invalidate_after is not None:
             delta = datetime.timedelta(seconds=int(invalidate_after))
-            if info['created'] < datetime.datetime.utcnow() - delta:
-                logger.info("Skipping cache because cache image is too old.")
+            if image.created < datetime.datetime.utcnow() - delta:
+                self.run.debug("Skipping {} cache because cache image is too old."
+                               .format(image))
                 return False
 
-        return info
+        return True
 
     def execute(self):
-        if self.cacheable:
-            info = self.can_cache()
-            if info:
-                self.run.log("==> Using cached image id {}".format(info['id'][:12]), color="OKBLUE")
-                return info['id']
+        if self.cacheable and self.cached_images:
+            self.run.log("Found {} cached image(s) from previous run".format(len(self.cached_images)))
+            self.cached_images = [i for i in self.cached_images if self.valid_cache(i)]
+            # Use the most recently generated of valid cache images
+            self.cached_images.sort(key=lambda img: img.created, reverse=True)
+            if self.cached_images:
+                image = self.cached_images[0]
+                self.run.log("==> Using cached {}, saved {:.2f}".format(image, image.runtime), color="OKBLUE")
+                return image
 
         return self._execute()
 
@@ -123,12 +163,17 @@ class Step(object):
 
     def commit_container(self, container_id):
         assert len(container_id) == 64
-        self.labels['potter-runtime'] = str(time.time() - self.start_time)
-        image = self.run.client.commit(container=container_id, conf={'Labels': self.labels})
-        assert len(image['Id']) == 64
+        self.labels['potter_runtime'] = str(time.time() - self.start_time)
+        resp = self.run.client.commit(container=container_id, conf={'Labels': self.labels})
         self.run.client.remove_container(container=container_id)
-        self.run.log("==> New image with hash {} and labels {}".format(image['Id'][:12], self.labels), color="OKGREEN")
-        return image['Id']
+        resp = self.run.client.inspect_image(image=resp['Id'])
+        image = Image.from_inspect(resp)
+        self.run.log("==> New image {} generated in {}".format(image, image.runtime), color="OKGREEN")
+        return image
+
+    @property
+    def config_hash(self):
+        return str(hash(json.dumps(self.config)))
 
 
 class Command(Step):
@@ -137,7 +182,7 @@ class Command(Step):
             command = self.config.get('join', " && ").join(self.config['run'])
         else:
             command = self.config['run']
-        container = self.run.client.create_container(image=self.target_image, command=["/bin/bash", "-c", command])
+        container = self.run.client.create_container(image=self.target_image.id, command=["/bin/bash", "-c", command])
         self.run.client.start(container['Id'])
         for log in self.run.client.attach(container=container['Id'], stdout=True, stderr=True, stream=True, logs=True):
             sys.stdout.write(log)
@@ -147,7 +192,7 @@ class Command(Step):
 
 class Copy(Step):
     def _execute(self):
-        container = self.run.client.create_container(image=self.target_image)
+        container = self.run.client.create_container(image=self.target_image.id)
         uploadpath = os.path.join(self.config['dest'], os.path.basename(self.config['source']))
         self.run.log("Creating temporary tar file to upload {} to {}"
                      .format(self.config['source'], uploadpath))
@@ -198,11 +243,13 @@ class Pull(Step):
                     progress = False
                     sys.stdout.write('\n')
                 print(data['status'])
+        self.start_time = time.time()  # Don't count the pull time as part of runtime
         self.run.log("==> Using image {}:{} as base".format(self.config['image'], tag), color="OKGREEN")
-        return self.config['image']
+        container = self.run.client.create_container(image=self.config['image'])
+        return self.commit_container(container['Id'])
 
 
-if __name__ == "__main__":
+def main():
     console = logging.StreamHandler()
     console.setLevel(logging.DEBUG)
     formatter = logging.Formatter('%(message)s')
@@ -215,3 +262,6 @@ if __name__ == "__main__":
 
     potter = Run(**vars(parser.parse_args()))
     potter.run()
+
+if __name__ == "__main__":
+    main()
