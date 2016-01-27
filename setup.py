a6cac457c10825f00a29f3818da1fe6c90c21587
Isaac Cook <isaac@simpload.com>
2016-01-27 00:32:15 -0600

    Make installation create a binary

--- setup.py
+++ setup.py
@@ -4,5 +4,10 @@ from setuptools import setup, find_packages
 
 setup(name='potter',
       version='0.1.0',
-      packages=find_packages()
+      packages=find_packages(),
+      entry_points={
+          'console_scripts': [
+              'potter = potter:main'
+          ]
+      }
       )
