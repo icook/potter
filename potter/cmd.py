e1c8acaee0d8ca3336e36bd2d70c0c9971025ef8
Isaac Cook <isaac@simpload.com>
2016-02-17 01:04:43 -0600

    Add version info to command line args

--- potter/cmd.py
+++ potter/cmd.py
@@ -3,6 +3,7 @@ import sys
 import argparse
 
 from . import build
+from . import __version__
 
 
 class StoreNameValuePair(argparse.Action):
@@ -17,6 +18,7 @@ def main():
     parser = argparse.ArgumentParser(description='Build a docker container from a potter config file')
     parser.add_argument('config_file', help='the configuration file to load', type=argparse.FileType('r'))
     parser.add_argument('command', choices=['build', 'info'])
+    parser.add_argument('--version', '-v', help='version information', action='store_true')
     parser.add_argument('--log-level', '-l', help='how verbose to log',
                         choices=['DEBUG', 'INFO', 'WARN', 'ERROR'], default='INFO')
     parser.add_argument('--context', help='key value pairs to feed to jinja in key=val format', action=StoreNameValuePair)
@@ -29,6 +31,10 @@ def main():
     logger = logging.getLogger('potter')
     logger.addHandler(console)
     logger.setLevel(getattr(logging, args.log_level))
+
+    if args.version is True:
+        print(__version__)
+        return 0
 
     potter = build.Build(**vars(args))
     if args.command == 'build':
